ec45c8506b749fc1e9529582852289d1e97f125f
mahmoud.elrefaie@gmail.com <mahmoud.elrefaie@gmail.com@fca4a5e7-8541-616f-11ec-a1b6e0ed4f86>
2011-04-01 19:16:59 +0000

    added  all the servlets to the newly created packages, servlets so that entries at web.xml can be modified

--- trunk/Ecommerce_JSP/src/java/main/DataEntry.java
+++ trunk/Ecommerce_JSP/src/java/main/DataEntry.java
@@ -5,9 +5,13 @@
 
 package main;
 import databeans.*;
+import databeans.model.AdminService;
+import databeans.model.AdminService_Interface;
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
 import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -21,24 +25,38 @@ import org.hibernate.criterion.Restrictions;
  */
 public class DataEntry {
     public static void main(String[] args) {
-        SessionFactory factory = new Configuration().configure().buildSessionFactory();
-        Session session = factory.openSession();
-        Categories cat = new Categories("Planes");
-
-        Products prdct = new Products(cat, "Phantom", 10000, "no image");
-        Set set = cat.getProductses();
-        set.add(prdct);
-        cat.setProductses(set);
-//        cat.setProductses();
-        session.beginTransaction();
-        session.persist(cat);
-        session.getTransaction().commit();
-
+        AdminService_Interface admin = AdminService.getServiceInstance();
+        Categories cat = new Categories("books");
+        cat.setId(10);
+        //admin.addProduct(new Products(cat, "7 hats", 100, "none"));
+        admin.addCategory(new Categories("egypt"));
+//        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+//        Session session = factory.openSession();
+//        Categories cat = new Categories("Planes");
+//
+//        Products prdct = new Products(cat, "Phantom", 10000, "no image");
+//        Set set = cat.getProductses();
+//        set.add(prdct);
+//        cat.setProductses(set);
+////        Users user = new Users("mahmoud", "m.el_refaie@yahoo.com", "123");
+////        user.setDob(new Date(88, 0, 1));
+////        user.setInterests("basketball");
+////        user.setJob("Engineer");
+//        String queryString = "FROM Users u WHERE u.uid=:user_id";
+//        Query q = session.createQuery(queryString);
+//        q.setInteger("user_id", 1);
+//        Users user = (Users)q.list().get(0);
+//        user.setDob(new Date(88, 0, 1));
+////        cat.setProductses();
 //        session.beginTransaction();
-//        session.persist(prdct);
+//        session.persist(user);
 //        session.getTransaction().commit();
-
-        System.out.println("transaction submitted");
+//
+////        session.beginTransaction();
+////        session.persist(prdct);
+////        session.getTransaction().commit();
+//
+//        System.out.println("transaction submitted");
     }
 
 }
