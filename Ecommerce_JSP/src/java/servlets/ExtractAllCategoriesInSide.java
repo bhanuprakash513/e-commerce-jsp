10981286daa98bf2e69f1a1b2c4c8a844a5eb341
islam.farid2100 <Islam.Farid2100@gmail.com>
2011-04-02 10:04:28 +0000

    gadeeeeeed

--- Ecommerce_JSP/src/java/servlets/ExtractAllCategoriesInSide.java
+++ Ecommerce_JSP/src/java/servlets/ExtractAllCategoriesInSide.java
@@ -1,36 +1,28 @@
-package servlets;
-
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 
+package servlets;
 
 import databeans.Categories;
-import databeans.Products;
-import databeans.model.AdminService;
-import databeans.model.AdminService_Interface;
+import databeans.model.UserService;
+import databeans.model.UserService_Interface;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import pagesbeans.ProductBean;
 
 /**
  *
  * @author islam
  */
-/**
- * this servlet add the product to the db and do the work of the jsp(productaddingindb) in order not to write a acod in the jsp page
- * @author islam
- */
-public class AddProductToDb extends HttpServlet {
-   Products pdata=new Products();
-   ProductBean ppage;
-   Categories c;
-   AdminService_Interface admin=AdminService.getServiceInstance();
+public class ExtractAllCategoriesInSide extends HttpServlet {
+   UserService_Interface user=UserService.getServiceInstance();
+   ArrayList<Categories> categories=user.getAllCategories();
     /** 
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
@@ -43,24 +35,17 @@ public class AddProductToDb extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
-            ppage=(ProductBean)request.getAttribute("product");
-            pdata.setDescription(ppage.getDescription());
-            pdata.setName(ppage.getProductName());
-            pdata.setPrice(ppage.getPriceValue());
-            pdata.setPictureLink(ppage.getPicturePath());
-            pdata.setQuantity(ppage.getQuantity());
-            c=admin.getCategory(ppage.getCategory());
-            pdata.setCategories(c);
-            admin.addProduct(pdata);
-            //pdata.setName(null);
+            for(Categories c:categories){
+                out.println("<li><a href='#'>"+c.getName()+"</a></li>");
 
+            }
             /* TODO output your page here
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet AddProductToDb</title>");  
+            out.println("<title>Servlet ExtractAllCategoriesInSide</title>");  
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet AddProductToDb at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet ExtractAllCategoriesInSide at " + request.getContextPath () + "</h1>");
             out.println("</body>");
             out.println("</html>");
             */
