27a5b2f364033f6e56513821c2632c0c8639f0d2
islam.farid2100 <Islam.Farid2100@gmail.com>
2011-04-02 18:38:45 +0000

    yarab yeg3al t3abna be fayda :D

--- Ecommerce_JSP/src/java/servlets/EditProduct.java
+++ Ecommerce_JSP/src/java/servlets/EditProduct.java
@@ -24,8 +24,8 @@ import javax.servlet.http.HttpServletResponse;
  * @author islam
  */
 public class EditProduct extends HttpServlet {
-
-    /**
+   
+    /** 
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
      * @param response servlet response
@@ -60,20 +60,20 @@ public class EditProduct extends HttpServlet {
             /* TODO output your page here
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet EditProduct</title>");
+            out.println("<title>Servlet EditProduct</title>");  
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet EditProduct at " + request.getContextPath () + "</h1>");
             out.println("</body>");
             out.println("</html>");
             */
-        } finally {
+        } finally { 
             //out.close();
         }
-    }
+    } 
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
+    /** 
      * Handles the HTTP <code>GET</code> method.
      * @param request servlet request
      * @param response servlet response
@@ -84,9 +84,9 @@ public class EditProduct extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
         processRequest(request, response);
-    }
+    } 
 
-    /**
+    /** 
      * Handles the HTTP <code>POST</code> method.
      * @param request servlet request
      * @param response servlet response
@@ -99,7 +99,7 @@ public class EditProduct extends HttpServlet {
         processRequest(request, response);
     }
 
-    /**
+    /** 
      * Returns a short description of the servlet.
      * @return a String containing servlet description
      */
