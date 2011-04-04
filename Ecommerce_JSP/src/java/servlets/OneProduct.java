9039885d0b6fec7226188f29889c0a0ccc1c3bb3
mohammed.yousry2 <mohammed.yousry2@gmail.com>
2011-04-04 04:09:10 +0000

    add oneproduct add product

--- Ecommerce_JSP/src/java/servlets/OneProduct.java
+++ Ecommerce_JSP/src/java/servlets/OneProduct.java
@@ -4,7 +4,7 @@
  */
 
 package servlets;
-
+import databeans.Products;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -29,18 +29,48 @@ public class OneProduct extends HttpServlet {
     throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
+
+
+        int productId = Integer.valueOf(request.getParameter("productId"));
+
+
+        Products p = new Products();
+        p.setDescription("sdfjknkjnknklnjlbnkljblbiklnbj jnsjknckdsnfijsdnfisdnfinsdifdsifusd dsnfisndifinsinsnf "
+                + "fdsnfsoidfnsfnsfdnsufns");
+        p.setPrice(6);
+        p.setProductId(productId);
+        p.setName("sfd");
+        p.setQuantity(60);
+        p.setPictureLink("http://localhost:8081/delete/images/1.jpg");
+
         try {
-            /* TODO output your page here
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet OneProduct</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet OneProduct at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-            */
-        } finally { 
+            out.println("<div class=\"post\">");
+                out.println("<h2 class=\"title\"><p> <a href=\"\">"+p.getName()+"</a></h2>");
+                out.println("<div class=\"entry\">");
+                out.print("<form action= AddToCart.jsp?productId="+p.getProductId()+" method = \"POST\">"
+                        + "<p align = center ><img src="+p.getPictureLink()+ " height = 150 width =150 /></p>"
+                        +"<strong> Price = " + p.getPrice()+"</strong>"+ " Discription : "
+                        +p.getDescription());
+                out.println("<p align = right>"
+                        + "<select name=quantity size=1>");
+                out.println("<option name= "+0+" value= \""+1+"\">Quantity</option>");
+
+                for(int x =1;x<p.getQuantity()+1;x++)
+                {
+                    out.println("<option name= "+x+" value= "+x+">"+x+" </option>");
+                }
+                out.print("</select>");
+		out.println("<input type =\"submit\" value = \"Add to Cart\"/>");
+                out.println("</form></div>");
+		out.println("</div>");
+
+
+
+        }
+
+
+
+        finally {
             out.close();
         }
     } 
